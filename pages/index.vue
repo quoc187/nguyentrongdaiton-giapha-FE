c8ff025b673a65c37bbaee219c7f8de6fa4c9615
quoc187 <nguyentrongquocbk@gmail.com>
2024-08-14 21:56:58 +0700

    feat: cloudflare pages

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,9 @@
 <script setup lang="ts">
   import AddMe from '~/features/add/add-me.vue';
 
-  const count = ref(0)
-
-  const increase = () => {
-    count.value++;
-  }
+  useHead({
+    title: "Gia pha nguyen trong",
+  })
 </script>
 
 <template>
